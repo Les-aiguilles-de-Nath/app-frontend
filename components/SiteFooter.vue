b74dbe8e30efce638a160f98437be68d2008ee19
Mickaël Dhainaut <mickael@popsell.com>
2022-06-18 00:24:04 +0200

    ✨ Made site footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,28 @@
 <template>
-    <footer>
-        <div class="w-100 text-center">
+    <footer class="w-full px-5 py-7">
+        <div class="text-center">
+            <p>Copyright © - Les aiguilles de Nath {{ year }}</p>
             <p>Made with love by Mickaël Dhainaut</p>
         </div>
     </footer>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    computed: {
+        year() {
+            return new Date().getFullYear();
+        }
+    }
+})
+</script>
+
+
+<style lang="scss" scoped>
+footer {
+    background: $grey;
+    color: $white;
+}
+</style>
