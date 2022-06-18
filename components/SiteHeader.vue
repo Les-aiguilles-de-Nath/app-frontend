4065656b6dcce9e0a8b940aa5144ff8bbddcf0c9
Mickaël Dhainaut <mickael@popsell.com>
2022-06-18 11:18:33 +0200

    🩹 Fixed header z-index

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="flex justify-between px-4 py-2 w-full">
+    <header class="site-header flex justify-between px-4 py-2 w-full">
         <a class="flex justify-start items-center" href="/">
             <img class="w-20 mr-2" src="/images/logos/logo.png" alt="Les aiguilles de Nath">
             <p class="logo-watermark flex flex-col justify-center">
@@ -24,17 +24,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.logo-watermark {
-    font-family: $font_display;
-    font-size: $font_large;
-    line-height: 1;
-    color: $contrast;
 
-    span {
-        &:nth-child(2) {
-            position: relative;
-            left: 16px;
-            font-size: $font_vlarge;
+.site-header {
+    z-index: 200;
+    .logo-watermark {
+        font-family: $font_display;
+        font-size: $font_large;
+        line-height: 1;
+        color: $contrast;
+
+        span {
+            &:nth-child(2) {
+                position: relative;
+                left: 16px;
+                font-size: $font_vlarge;
+            }
         }
     }
 }
